3a757ca58bfd3cabf6b4f7fd86156fc0b7ce32df
Manji Coder <manjicoder@gmail.com>
2024-12-03 02:04:29 +0530

    navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,6 +2,7 @@ import Logo from '@/assets/img/Brandlogo.png';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 const navLinks = [
   {
@@ -24,17 +25,42 @@ const navLinks = [
 
 export default function Navbar() {
   const { pathname } = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <header className='grid grid-cols-1 md:grid-cols-[30%,70%] shadow-lg backdrop-blur-sm sticky z-10 top-0 bg-white/50 items-center space-x-16 text-lg font-medium'>
-      {/* Logo */}
-      <Image
-        className='h-16 max-sm:w-full  aspect-video'
-        src={Logo}
-        alt='Logo'
-        width={400}
-        height={50}
-      />
-
+    <header className='grid grid-cols-1 md:grid-cols-[30%,70%] shadow-lg backdrop-blur-sm sticky z-10 top-0 bg-white/75 md:bg-white/50 items-center space-x-16 text-lg font-medium'>
+      <div className='flex justify-between'>
+        {/* Logo */}
+        <Image
+          className='h-16 max-sm:w-full  aspect-video'
+          src={Logo}
+          alt='Logo'
+          width={400}
+          height={50}
+        />
+        {/* Toggle Button For Moblie only */}
+        <button className='md:hidden w-16' onClick={() => setIsOpen(!isOpen)}>
+          {!isOpen ? 'open' : 'close'}
+        </button>
+      </div>
+        {isOpen && (
+          <nav className='md:hidden !m-0'>
+            <ul className='flex flex-col px-5 gap-y-3 py-10 overflow-auto text-lg font-medium '>
+              {navLinks.map(({ name, href }) => (
+                <Link
+                  href={href}
+                  key={href}
+                  className={`${
+                    pathname.includes(href)
+                      ? 'text-white bg-primary'
+                      : 'hover:bg-primary hover:text-white font-medium min-w-16'
+                  } border p-2 rounded-md shadow-sm`}
+                >
+                  {name}
+                </Link>
+              ))}
+            </ul>
+          </nav>
+        )}
       {/* NavLinks */}
       <nav className='hidden md:block'>
         <ul className='flex space-x-16 text-lg font-medium '>
