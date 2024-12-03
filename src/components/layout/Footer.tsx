b92277aa11107a204db9d7b04ac541eea11613b4
Manji Coder <manjicoder@gmail.com>
2024-12-03 17:21:18 +0530

    wp btn

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,17 +3,24 @@ import { PhoneIcon } from '@heroicons/react/20/solid';
 
 export default function Footer() {
   const year = new Date().getFullYear();
+  const phoneNumber = '7219844829';
+  const message = 'Hello, I have a question!';
+
   return (
     <footer className='text-center p-6'>
-      {/* CTA Fixed Button */}
+      {/* WhatsApp Fixed Button */}
       <a
-        href='tel:+917219844829'
+        href={`https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+          message
+        )}`}
+        target='_blank'
+        rel='noopener noreferrer'
         className='fixed bottom-10 right-6 bg-primary p-2 rounded-full border shadow-md cursor-pointer text-white animate-bounce hover:animate-none transition-all'
       >
         <PhoneIcon className='w-9' />
       </a>
+
       {/* Social Icons */}
-      <section></section>
 
       <p>
         ©Copyright {year}. All Rights Reserved. {appName}
